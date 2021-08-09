659898b19cb9edd8b416b87bfbdecc1d8526bc96
JoshDaos <joshuadaos1998@gmail.com>
2021-08-09 14:27:54 -0700

    added about component

--- portfolio-app/src/App.js
+++ portfolio-app/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Nav from './components/Nav/Nav'
 import Header from './components/Header/Header';
+import About from './components/About/About';
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <Header />
+      <About />
 
     </div>
   );
---
+++ portfolio-app/src/components/About/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function About() {
+    return (
+        <div className="about__container">
+            <p>About me</p>
+        </div>
+    )
+}
+
+export default About
