fe7e76de76378e5782cd80c8c74767bec47a4efe
JoshDaos <joshuadaos1998@gmail.com>
2021-08-19 14:27:58 -0700

    added projects component

--- portfolio-app/src/App.js
+++ portfolio-app/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Nav from './components/Nav/Nav'
 import Header from './components/Header/Header';
 import About from './components/About/About';
+import Projects from './components/Projects/Projects';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Nav />
       <Header />
       <About />
+      <Projects />
 
     </div>
   );
---
+++ portfolio-app/src/components/Projects/Projects.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Projects() {
+    return (
+        <div className="projects__container">
+            Projects
+        </div>
+    )
+}
+
+export default Projects
