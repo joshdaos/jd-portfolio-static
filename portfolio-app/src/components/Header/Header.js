3a13868c1f3d2621fc7276003eb3bb1836ee0498
JoshDaos <joshuadaos1998@gmail.com>
2021-09-18 12:44:40 -0700

    added some content in intro

--- portfolio-app/src/components/Header/Header.js
+++ portfolio-app/src/components/Header/Header.js
@@ -3,7 +3,9 @@ import React from 'react'
 function Header() {
     return (
         <div id="header" className="header__container">
-            <p>Hello there!</p>
+            <h3>Joshua R. Daos<br></br></h3>
+            <h3>Software Engineer<br></br></h3>
+            <a className="anchor" href="#project"><h4>PROJECTS</h4></a>
         </div>
     )
 }
