33a334cbe0efc4a753025dbe23f11edadd3b25d0
JoshDaos <joshuadaos1998@gmail.com>
2021-09-25 19:48:04 -0700

    some content in about container

--- portfolio-app/src/components/About/About.js
+++ portfolio-app/src/components/About/About.js
@@ -3,7 +3,15 @@ import React from 'react'
 function About() {
     return (
         <div id="about" className="about__container">
-            <p>About me</p>
+            <h3>About me</h3>
+            <p>
+                Hello, my name is Josh Daos.<br></br>
+                I am a Software Engineer that is based in the Bay Area.<br></br>
+                I always had a knack for tech, but never imagined to work in tech until a year ago. <br></br>
+                I was exposed to programming in late 2019, where a relative showed me a project. <br></br>
+                I became intrigued from how he was able to create and develop something. <br></br>
+                My hunger to know more about this space was birthed.
+            </p>
         </div>
     )
 }
